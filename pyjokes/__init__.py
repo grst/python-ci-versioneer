7ad75aa628dcb7bc374ff82d675294a6b7febc5d
Gregor Sturm <mail@gregor-sturm.de>
2020-05-22 15:26:16 +0200

    Add placeholder version number

--- pyjokes/__init__.py
+++ pyjokes/__init__.py
@@ -4,13 +4,13 @@ from __future__ import absolute_import
 from .pyjokes import get_joke, get_jokes
 
 
-__project__      = 'pyjokes'
-__version__      = '0.6.0'
-__keywords__     = ['pyjokes', 'pyjokes', 'jokes', 'joke']
-__author__       = 'Pyjokes Society'
-__author_email__ = 'ben@bennuttall.com'
-__url__          = 'https://pyjok.es/'
-__platforms__    = 'ALL'
+__project__ = "pyjokes"
+__version__ = "develop"
+__keywords__ = ["pyjokes", "pyjokes", "jokes", "joke"]
+__author__ = "Pyjokes Society"
+__author_email__ = "ben@bennuttall.com"
+__url__ = "https://pyjok.es/"
+__platforms__ = "ALL"
 
 __classifiers__ = [
     "Development Status :: 4 - Beta",
@@ -21,15 +21,15 @@ __classifiers__ = [
 ]
 
 __entry_points__ = {
-    'console_scripts': [
-        'pyjoke = pyjokescli.pyjoke:main',
-        'pyjokes = pyjokescli.pyjokes:main',
+    "console_scripts": [
+        "pyjoke = pyjokescli.pyjoke:main",
+        "pyjokes = pyjokescli.pyjokes:main",
     ],
 }
 
 __requires__ = []
 
 __extra_requires__ = {
-    'doc':   ['mkdocs'],
-    'test':  ['pytest', 'coverage', 'tox'],
+    "doc": ["mkdocs"],
+    "test": ["pytest", "coverage", "tox"],
 }
